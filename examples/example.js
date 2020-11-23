c0e66d3697c5f2b5c983e5f67e9e14755102733c
wizar-d <63768019+wizar-d@users.noreply.github.com>
2020-11-23 03:00:45 -0500

    made important changes
    10x better now

--- examples/example.js
+++ examples/example.js
@@ -5,6 +5,7 @@ async function Main() {
     console.log("Using Email : " + Email)
     var Emails = await tempmailoapi.getEmails(Email);
     console.log("Found " + Emails.length + " Email(s) on " + Email + "!");
+    console.log("nuts");
     Emails.forEach(element => console.log("From : " + element['from'] + " | Subject : " + element['subject']));
 }
 
