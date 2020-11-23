5253f51c5828da3d3404a5894a34059305c0fa48
Proto <42853809+ProtoGrace@users.noreply.github.com>
2020-11-23 15:00:30 -0500

    Replace Math.floors with tildes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 function randomString(length, chars) {
     var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    for (var i = length; i > 0; --i) result += chars[~~(Math.random() * chars.length)];
     return result;
 }
 
@@ -54,7 +54,7 @@ async function getEmails(email) {
 
 function genRandomEmail() {
     const EmailDomains = ["livinginsurance.eu", "ivyandmarj.com", "digital10network.com", "celinecityitalia.com"];
-    const RandomName = randomString(Math.floor(Math.random() * (8 - 5) + 5), 'abcdefghijklmnopqrstuvwxyz');
+    const RandomName = randomString(~~(Math.random() * (8 - 5) + 5), 'abcdefghijklmnopqrstuvwxyz');
     const Email = RandomName + "@" + EmailDomains[~~(EmailDomains.length * Math.random())];
     return Email;
 }
