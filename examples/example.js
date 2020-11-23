98e452679fed86f9fa150c099d48333b0385621e
Proto <42853809+ProtoGrace@users.noreply.github.com>
2020-11-23 02:30:45 -0500

    Uncap this

--- examples/example.js
+++ examples/example.js
@@ -3,7 +3,7 @@ const tempmailoapi = require('tempmailo-api');
 async function Main() {
     var Email = tempmailoapi.genRandomEmail();
     console.log("Using Email : " + Email)
-    var Emails = await tempmailoapi.GetEmails(Email);
+    var Emails = await tempmailoapi.getEmails(Email);
     console.log("Found " + Emails.length + " Email(s) on " + Email + "!");
     Emails.forEach(element => console.log("From : " + element['from'] + " | Subject : " + element['subject']));
 }
